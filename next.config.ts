990a8aa3457c44770b0e450dead30f6f097f1415
Vinicius-Castilho <vinicius.c.m.amorim@gmail.com>
2026-05-12 14:16:36 -0300

    Next Config alteracao

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* Libera o acesso do seu IP na rede local para o WebSocket funcionar no celular */
-  allowedDevOrigins: ['10.0.3.241'],
+  ...(process.env.NODE_ENV === 'development' && {
+    allowedDevOrigins: ['10.0.3.241'],
+  }),
 };
 
 export default nextConfig;
